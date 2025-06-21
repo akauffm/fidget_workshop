cd1153893be975d96c6b56202a51a36a1fcd6634
akauffm <akauffm@users.noreply.github.com>
2025-06-21 09:23:54 -0700

    Update SDXLTurboClient.py

--- SDXLTurboClient.py
+++ SDXLTurboClient.py
@@ -1,3 +1,7 @@
+# A minimal client to interact with a model served via Pinokio
+# The prediction part will be specific to the app, click on the
+# API tab at the bottom of the Gradio UI to see the particulars.
+
 from gradio_client import Client
 
 client = Client("http://127.0.0.1:7860/")
