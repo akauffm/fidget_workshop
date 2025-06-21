f5e15a368190bf81a5803836ee1e0c9b0552f0a2
akauffm <akauffm@users.noreply.github.com>
2025-06-21 09:22:20 -0700

    Add files via upload

---
+++ SDXLTurboClient.py
@@ -0,0 +1,9 @@
+from gradio_client import Client
+
+client = Client("http://127.0.0.1:7860/")
+result = client.predict(
+		"A pig with wings",	# str  in 'Prompt' Textbox component
+		None,	# str (filepath on your computer (or URL) of image) in 'image' Image component
+		api_name="/predict"
+)
+print(result)
